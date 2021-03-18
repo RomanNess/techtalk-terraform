4e0c6d09b389448a2072bf252c57c7954c8432df
Roman Ness <roman.ness@cosee.biz>
2021-03-18 22:31:44 +0100

    add tf-backend stack

---
+++ tf-backend/remote_state.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region              = "eu-central-1"
+  allowed_account_ids = [640847388391]
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_kms_alias" "s3" {
+  name = "alias/aws/s3"
+}
+
+resource "aws_s3_bucket" "terraform_remote_state" {
+  bucket = "techtalk-terraform-remote-state-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = data.aws_kms_alias.s3.arn
+      }
+    }
+  }
+
+  tags = {
+    stage     = "nostage"
+    stack     = "tf-backend"
+    managedby = "terraform"
+  }
+}
+
+output "remote_state_bucket_arn" {
+  value = aws_s3_bucket.terraform_remote_state.arn
+}
