25e5d1ad3d78c8deed30eda8009ef30c0440f5a6
Roman Ness <roman.ness@cosee.biz>
2021-03-20 11:56:59 +0100

    add api gateway to lambda stack

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -4,8 +4,29 @@ data "archive_file" "main_zip" {
   output_path = "${path.module}/build/main.zip"
 }
 
+resource "aws_iam_role" "lambda" {
+  name = "hello_terraform"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda.json
+
+}
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+      type        = "Service"
+    }
+  }
+}
+
 resource "aws_lambda_function" "lambda" {
-  function_name = "terraform"
+  function_name = "hello_terraform"
 
   filename = data.archive_file.main_zip.output_path
   source_code_hash = data.archive_file.main_zip.output_base64sha256
